652f4ce1c875724fd78c969864bc4e5d0a558ad1
D J Tuinhof <Denice@wcw-stud-145-109-21-12.wireless.uva.nl>
2015-10-26 16:50:48 +0100

    First commit

--- Tweedepoging/ViewController.swift
+++ Tweedepoging/ViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    
+    var result = Float ()
+    var currentNumber = Float()
+    
+    var currentOp = String ()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        currentOp = "="
+        lblResult.text = ("\(result)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func btnNumberInput(sender: UIButton) {
+        currentNumber = currentNumber * 10 + Float(sender.titleLabel!.text!)!
+        lblResult.text = ("\(currentNumber)")
+    }
+    
+    func textFieldShouldReturn(nameTextField: UITextField) -> Bool {
+        nameLabel.textColor = UIColor.redColor()
+        nameLabel.text = "Hello \(nameTextField.text!)"
+        return true
+    }
 
+    @IBAction func btnOperation(sender: UIButton) {
+        switch currentOp {
+            case "=":
+                result = currentNumber
+            case "+":
+                result = result + currentNumber
+            case "-":
+                result = result - currentNumber
+            case "*":
+                result = result * currentNumber
+            case "/":
+                result = result / currentNumber
+            default:
+                print("error")
+        }
+        currentNumber = 0
+        lblResult.text = ("\(result)")
+        
+        if(sender.titleLabel!.text == "=") {
+            result = 0
+        }
+        currentOp = sender.titleLabel!.text! as String!
+    }
+    
+    @IBAction func btnClear(sender: UIButton) {
+        result = 0
+        currentNumber = 0
+        currentOp = "="
+        lblResult.text = ("\(result)")
 
+    }
+    
 }
 
